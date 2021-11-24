aa0841ddd33bb2a77b5f510335068d20e864b43d
Brian Sanchez <bsanchezvazquez@iessonferrer.net>
2021-11-24 16:45:51 +0100

    def

--- ProyectoJS/ejercicio1.js
+++ ProyectoJS/ejercicio1.js
@@ -11,12 +11,11 @@ Edificio.prototype.agregarPlanta = function (numeroPlanta) {
 }
 
 Edificio.prototype.agregarPuerta = function (numeroPlanta, numeroPuerta) {
-    this.mapaPropietariosEdificio.get(numeroPlanta).set(numeroPuerta, array = []);
+    this.mapaPropietariosEdificio.get(numeroPlanta).set(numeroPuerta, []);
 }
 
 Edificio.prototype.agregarPropietario = function (nombrePropietario, numeroPlanta, numeroPuerta) {
-    this.mapaPropietariosEdificio.get(numeroPlanta).set(numeroPuerta, array = (array[array.lenght] = nombrePropietario));
-    console.log(array);
+    this.mapaPropietariosEdificio.get(numeroPlanta).get(numeroPuerta).push(nombrePropietario);
 }
 
 Edificio.prototype.imprimirCodigoPostal = function () {
@@ -36,7 +35,27 @@ Edificio.prototype.imprimirTipoVia = function () {
 }
 
 Edificio.prototype.imprimirTodosPropietarios = function () {
-    return "Propietarios: " + "\n";
+    let clavePlanta = this.mapaPropietariosEdificio.keys();
+    let texto = "<center><div><h2>COMUNITAT DE PROPIETARIS</h2><br>" +
+        this.tipoVia + " " + this.nombreVia + ", " + this.numeroEdificio +
+        " C.P " + this.codigoPostal +
+        "</div></center><br>";
+    for (let i = 0; i < this.mapaPropietariosEdificio.size; i++) {
+        let planta = clavePlanta.next().value;
+        let clavePuerta = this.mapaPropietariosEdificio.get(planta).keys()
+        texto += "<h3>Planta: " + planta + "/<h3>\n";
+
+        for (let x = 0; x < this.mapaPropietariosEdificio.get(planta).size; x++) {
+            let puerta = clavePuerta.next().value
+            texto += "\t" + "<h3>Puerta: " + puerta + "</h3>\n";
+
+            for (let j = 0; j < this.mapaPropietariosEdificio.get(planta).get(puerta).length; j++) {
+                let propietario = this.mapaPropietariosEdificio.get(planta).get(puerta);
+                texto += "\t\t" + propietario[j] + "\n";
+            }
+        }
+    }
+    return texto;
 }
 
 Edificio.prototype.modificarCodigoPostal = function (nuevoCodigoPostal) {
@@ -54,148 +73,3 @@ Edificio.prototype.modificarNumeroEdificio = function (nuevoNumeroEdificio) {
 Edificio.prototype.modificarTipoVia = function (nuevoTipoVia) {
     this.tipoVia = nuevoTipoVia;
 }
-
-const edificio1 = new Edificio('calle', 'marques de la fontsanta', 60, '07005');
-console.log(edificio1)
-/* SALIDA:
-    Edificio {
-    tipoVia: 'calle',
-    nombreVia: 'marques rubio',
-    numeroEdificio: 70,
-    codigoPostal: '07005',
-    mapaPropietariosEdificio: Map(0) {},
-    modificarTipoVia: [Function (anonymous)],
-    modificarNombreVia: [Function (anonymous)],
-    modificarNumeroEdificio: [Function (anonymous)],
-    modificarCodigoPostal: [Function (anonymous)],
-    imprimirTipoVia: [Function (anonymous)],
-    imprimirNombreVia: [Function (anonymous)],
-    imprimirNumeroEdificio: [Function (anonymous)],
-    imprimirCodigoPostal: [Function (anonymous)],
-    agregarPlanta: [Function (anonymous)],
-    agregarPuerta: [Function (anonymous)],
-    agregarPropietario: [Function (anonymous)],
-    imprimirTodosPropietarios: [Function (anonymous)]
-    }
-*/
-
-edificio1.agregarPlanta('1A');
-edificio1.agregarPlanta('1B');
-console.log(edificio1);
-/* SALIDA:
-    Edificio {
-    tipoVia: 'calle',
-    nombreVia: 'marques rubio',
-    numeroEdificio: 70,
-    codigoPostal: '07005',
-    mapaPropietariosEdificio: Map(2) { '1A' => Map(0) {}, '1B' => Map(0) {} },
-    modificarTipoVia: [Function (anonymous)],
-    modificarNombreVia: [Function (anonymous)],
-    modificarNumeroEdificio: [Function (anonymous)],
-    modificarCodigoPostal: [Function (anonymous)],
-    imprimirTipoVia: [Function (anonymous)],
-    imprimirNombreVia: [Function (anonymous)],
-    imprimirNumeroEdificio: [Function (anonymous)],
-    imprimirCodigoPostal: [Function (anonymous)],
-    agregarPlanta: [Function (anonymous)],
-    agregarPuerta: [Function (anonymous)],
-    agregarPropietario: [Function (anonymous)],
-    imprimirTodosPropietarios: [Function (anonymous)]
-    }
-*/
-
-edificio1.agregarPuerta('1A', '3');
-edificio1.agregarPuerta('1A', '2');
-edificio1.agregarPuerta('1B', '1')
-console.log(edificio1);
-/* SALIDA:
-    Edificio {
-    tipoVia: 'calle',
-    nombreVia: 'marques rubio',
-    numeroEdificio: 70,
-    codigoPostal: '07005',
-    mapaPropietariosEdificio: Map(2) {
-        '1A' => Map(2) { '3' => [], '2' => [] },
-        '1B' => Map(1) { '1' => [] }
-    },
-    modificarTipoVia: [Function (anonymous)],
-    modificarNombreVia: [Function (anonymous)],
-    modificarNumeroEdificio: [Function (anonymous)],
-    modificarCodigoPostal: [Function (anonymous)],
-    imprimirTipoVia: [Function (anonymous)],
-    imprimirNombreVia: [Function (anonymous)],
-    imprimirNumeroEdificio: [Function (anonymous)],
-    imprimirCodigoPostal: [Function (anonymous)],
-    agregarPlanta: [Function (anonymous)],
-    agregarPuerta: [Function (anonymous)],
-    agregarPropietario: [Function (anonymous)],
-    imprimirTodosPropietarios: [Function (anonymous)]
-    }
-*/
-
-edificio1.agregarPropietario('Belén Laserna Belenguer', '1A', '3');
-edificio1.agregarPropietario('Pedro Jimenez Vázquez', '1A', '3');
-edificio1.agregarPropietario('Ana María Rodriguez Figuerola', '1A', '2');
-edificio1.agregarPropietario('Miguel López López', '1B', '1');
-console.log(edificio1);
-/* SALIDA:
-    Edificio {
-    tipoVia: 'calle',
-    nombreVia: 'marques rubio',
-    numeroEdificio: 70,
-    codigoPostal: '07005',
-    mapaPropietariosEdificio: Map(2) {
-        '1A' => Map(2) { '3' => [Array], '2' => [Array] },
-        '1B' => Map(1) { '1' => [Array] }
-    },
-    modificarTipoVia: [Function (anonymous)],
-    modificarNombreVia: [Function (anonymous)],
-    modificarNumeroEdificio: [Function (anonymous)],
-    modificarCodigoPostal: [Function (anonymous)],
-    imprimirTipoVia: [Function (anonymous)],
-    imprimirNombreVia: [Function (anonymous)],
-    imprimirNumeroEdificio: [Function (anonymous)],
-    imprimirCodigoPostal: [Function (anonymous)],
-    agregarPlanta: [Function (anonymous)],
-    agregarPuerta: [Function (anonymous)],
-    agregarPropietario: [Function (anonymous)],
-    imprimirTodosPropietarios: [Function (anonymous)]
-    }
-*/
-
-console.log(edificio1.imprimirTodosPropietarios());
-/* SALIDA:
-    Planta: 1A
-        Puerta: 3
-            Belén Laserna Belenguer
-            Pedro Jimenez Vázquez
-        Puerta: 2
-            Ana María Rodriguez Figuerola
-    Planta: 1B
-        Puerta: 1
-            Miguel López López
-*/
-
-edificio1.modificarTipoVia('avenida');
-edificio1.modificarNombreVia('Marques de la Fontsanta');
-edificio1.modificarNumeroEdificio('60A');
-edificio1.modificarCodigoPostal('007005');
-console.log(edificio1.imprimirTipoVia());
-/* SALIDA:
-    Tipo de via: avenida
-*/
-
-console.log(edificio1.imprimirNombreVia());
-/* SALIDA:
-    Nombre de la vía: Marques de la Fontsanta
-*/
-
-console.log(edificio1.imprimirNumeroEdificio());
-/* SALIDA:
-    Número del edificio 60A
-*/
-
-console.log(edificio1.imprimirCodigoPostal());
-/* SALIDA:
-    Código Postal(CP): 007005
-*/
--- ProyectoJS/ejercicio2.js
+++ ProyectoJS/ejercicio2.js
@@ -7,3 +7,4 @@ function getRandomColor() {
     document.body.style.backgroundColor = color;
     
 }
+
