d2d135ec46cfb5db435806347028108752feedf9
Brian Sanchez <bsanchezvazquez@iessonferrer.net>
2021-11-24 20:58:26 +0100

    def

--- ProyectoJS/ejercicio3.mjs
+++ ProyectoJS/ejercicio3.mjs
@@ -1,7 +1,21 @@
-function notNull(valor){
-    if (valor < 0 || valor == null) {
-        return true;
-    }else{
-        return false;
+function datos() {
+    tipoVia = prompt("Tipo de via: ");
+    nombreVia = prompt("Nombre via: ");
+    numeroEdificio = prompt("Numero edificio: ");
+    codigoPostal = prompt("Codigo postal: ");
+    nombrePropietario = prompt("Nombre del propietario: ");
+    numeroPlanta = prompt("Numero de la planta: ");
+    numeroPuerta = prompt("Numero de la puerta: ");
+    piso = prompt("Piso: ");
+
+    puertaCheck = false;
+    while (puertaCheck == false) {
+        puerta = prompt("Puerta: ");
+
+        if (puerta < 0 || puerta == null) {
+            puertaCheck = true;
+        } else {
+            puertaCheck = false;
+        }
     }
 }
