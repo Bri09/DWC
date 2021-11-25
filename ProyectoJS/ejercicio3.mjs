84d0190ad408f6319b044b0890ab8cbe005b0c2f
Brian Sanchez <bsanchezvazquez@iessonferrer.net>
2021-11-25 19:17:31 +0100

    def

--- ProyectoJS/ejercicio3.mjs
+++ ProyectoJS/ejercicio3.mjs
@@ -1,32 +1,28 @@
 function datos() {
 
+    puertaCheck = false;
+
     tipoVia = prompt("Tipo de via: ");
     nombreVia = prompt("Nombre via: ");
     numeroEdificio = prompt("Numero edificio: ");
     codigoPostal = prompt("Codigo postal: ");
-    nombrePropietario = prompt("Nombre del propietario: ");
     numeroPlanta = prompt("Numero de la planta: ");
-    numeroPuerta = prompt("Numero de la puerta: ");
-    piso = prompt("Piso: ");
+    do {
+        numeroPuerta = prompt("Numero de la puerta: ");
+        if (numeroPuerta < 0 || numeroPuerta == "") {
+            puertaCheck = false;
+        } else {
+            puertaCheck = true;
+        }
+    } while (puertaCheck == false)
+    nombrePropietario = prompt("Nombre del propietario: ");
 
     alert("El tipo de via " + tipoVia + " se ha insertado\n" +
         "El nombre de la via " + nombreVia + " se ha insertado\n" +
         "El numero del edificio " + numeroEdificio + " se ha insertado\n" +
         "El codigo postal " + codigoPostal + " se ha insertado\n" +
-        "El nombre del propietario " + nombrePropietario + " se ha insertado\n" +
         "El numero de la planta " + numeroPlanta + " se ha insertado\n" +
         "El numero de la puerta " + numeroPuerta + " se ha insertado\n" +
-        "El piso " + piso + " se ha insertado");
-
-    puertaCheck = false;
-    do {
-        puerta = prompt("Puerta: ");
-        if (puerta < 0 || puerta == "") {
-            puertaCheck = false;
-        } else {
-            puertaCheck = true;
-            alert("La puerta " + puerta + " se ha insertado");
-        }
-    } while (puertaCheck == false)
+        "El nombre del propietario " + nombrePropietario + " se ha insertado");
 
 }
