4ba031a770c7c51502e36b16fdc39e5e628b44d9
Brian Sanchez <bsanchezvazquez@iessonferrer.net>
2021-11-26 17:30:44 +0100

    def

--- ProyectoJS/ejercicio3.mjs
+++ ProyectoJS/ejercicio3.mjs
@@ -1,21 +1,34 @@
 function datos() {
 
-    puertaCheck = false;
+    check = false;
 
     tipoVia = prompt("Tipo de via: ");
+    //alert("El tipo de via " + tipoVia + " se ha insertado\n");
+
     nombreVia = prompt("Nombre via: ");
+    //alert("El nombre de la via " + nombreVia + " se ha insertado\n");
+
     numeroEdificio = prompt("Numero edificio: ");
+    //alert("El numero del edificio " + numeroEdificio + " se ha insertado\n");
+
     codigoPostal = prompt("Codigo postal: ");
+    //alert("El codigo postal " + codigoPostal + " se ha insertado\n");
+
     numeroPlanta = prompt("Numero de la planta: ");
+    //alert("El numero de la planta " + numeroPlanta + " se ha insertado\n");
+
     do {
         numeroPuerta = prompt("Numero de la puerta: ");
-        if (numeroPuerta < 0 || numeroPuerta == "" /*|| isNaN(numeroPuerta) != false*/) {
-            puertaCheck = false;
+        if (numeroPuerta < 0 || numeroPuerta == "" /*|| isNaN(numeroPuerta) != false*/ ) {
+            check = false;
         } else {
-            puertaCheck = true;
+            check = true;
         }
-    } while (puertaCheck == false)
+    } while (check == false)
+    //alert("El numero de la puerta " + numeroPuerta + " se ha insertado\n");
+
     nombrePropietario = prompt("Nombre del propietario: ");
+    //alert("El nombre del propietario " + nombrePropietario + " se ha insertado");
 
     alert("El tipo de via " + tipoVia + " se ha insertado\n" +
         "El nombre de la via " + nombreVia + " se ha insertado\n" +
