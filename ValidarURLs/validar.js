72f31030c449ba6a4b71423fe1302a45f75353eb
Brian Sanchez <bsanchezvazquez@iessonferrer.net>
2021-11-29 17:38:50 +0100

    def

---
+++ ValidarURLs/validar.js
@@ -0,0 +1,11 @@
+function validateURL(textval) {
+    var urlregex = /^(https?|ftp):\/\/([a-zA-Z0-9.-]+(:[a-zA-Z0-9.&%$-]+)*@)*((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}|([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(:[0-9]+)*(\/($|[a-zA-Z0-9.,?'\\+&%$#=~_-]+))*$/;
+    return urlregex.test(textval);
+}
+
+texto = "http://stackoverflow.com/questions/1303872/url-validation-using-javascript";
+
+texto2 = "http://usuario:contrasenia@jarroba.com:80/artic/artic2/?okidoki";
+
+
+console.log(validateURL(texto2));
