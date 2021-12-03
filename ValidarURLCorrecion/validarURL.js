276726445ebf550de6c84a556ef56f5a5d411982
Brian Sanchez <bsanchezvazquez@iessonferrer.net>
2021-12-03 18:44:46 +0100

    def

--- ValidarURLCorrecion/validarURL.js
+++ ValidarURLCorrecion/validarURL.js
@@ -1,7 +1,8 @@
 // Obtención de la URL a comprobar
 var url = prompt('Indica la URL que quieres comprobar');
 
-url = 'http://usuario:contraseña@jarroba.com:80/articulos/unarticulo/?id=123&busqueda=java#comment-12345';
+//url = 'http://usuario:contraseña@jarroba.com:80/articulos/unarticulo/?id=123&busqueda=java#comment-12345';
+
 // Partes del patrón de la expresión regular
 patronProtocolo = `^(ftp|http|https):\\/{0,3}`;
 patronUsuarioPassword = `(([A-Za-z0-9\\-_\\.]*:.+|[A-Za-z0-9\\-_\\.]*)@+)*`;
