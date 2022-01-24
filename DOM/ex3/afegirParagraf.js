53ed58d79d2c84b50404f7ccc24c1e8773dc97fc
Brian Sanchez <bsanchezvazquez@iessonferrer.net>
2022-01-24 17:57:19 +0100

    "

--- DOM/ex3/afegirParagraf.js
+++ DOM/ex3/afegirParagraf.js
@@ -1,12 +1,11 @@
 var texto = prompt('Texto para el nuevo parágrafo:');
 var posicion = prompt('Posición del nuevo parágrafo:');
+var li = document.getElementsByTagName('li');
+console.log(li[0].innerHTML);
 
 function afegirParagraf() {
-    let li = document.getElementsByTagName('li');
+    li.splice(posicion, 0, texto)
     console.log(li);
-
-    for (let i = 0; i < li.length; i++) {
-        li.splice(posicion, 0, texto)
-
-    }
 }
+
+//afegirParagraf();
